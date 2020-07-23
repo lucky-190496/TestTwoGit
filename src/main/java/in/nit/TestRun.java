fe1cc7d819cb43d2c1b9153a6031ee6cd4bc6c76
lucky-190496 <lucky.gupta1904@gmail.com>
2020-07-23 14:57:09 +0530

    2 change

--- src/main/java/in/nit/TestRun.java
+++ src/main/java/in/nit/TestRun.java
@@ -1,5 +1,7 @@
 package in.nit;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.CommandLineRunner;
 
 public class TestRun implements CommandLineRunner {
@@ -7,6 +9,7 @@ public class TestRun implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	System.out.println("Continuous Learning!!!!");	
+	ArrayList<Integer> l=new ArrayList<>();
 	}
 
 }
