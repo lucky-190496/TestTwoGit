fc1d04e2674ee172c2118357f501caac09462660
lucky-190496 <lucky.gupta1904@gmail.com>
2020-07-23 14:54:17 +0530

    1 Time

---
+++ src/main/java/in/nit/TestRun.java
@@ -0,0 +1,12 @@
+package in.nit;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class TestRun implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+	System.out.println("Continuous Learning!!!!");	
+	}
+
+}
